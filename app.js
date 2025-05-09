bee41e648e80a050c2f4c9c6347abebc3caa421a
Gopal Mehtre <gopalmehtre10@gmail.com>
2025-05-09 10:59:05 +0530

    home page fix

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ const sessionOptions = {
     },
 };
 
-// app.get("/", (req, res)=>{
-//     res.send("works");
-// });
+app.get("/", (req, res)=>{
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
